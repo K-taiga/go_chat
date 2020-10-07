8445837e390eed9882b55af75fcddcdf2f399ac8
K-taiga <taigakatou0329@gmail.com>
2020-10-07 09:11:20 +0900

    add:commentary

--- server/main.go
+++ server/main.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 )
 
+// イベントがきっかけとして起動されるコールバック関数 = handler ハンドラ関数
+// がレスポンス返すインタフェースのResponseWirterとリクエストを受け取る構造体Request
 func handler(writer http.ResponseWriter, request *http.Request) {
   fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
 
+// 実行はこの関数から始まる
 func main() {
   http.HandleFunc("/", handler)
   http.ListenAndServe(":8080", nil)
